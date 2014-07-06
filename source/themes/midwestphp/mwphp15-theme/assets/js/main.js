c5501ee66ce820f44f5dd403acfc590926b51785
Jeremy Ward <jeremy@jmichaelward.com>
2014-07-06 12:57:07 -0500

    Add preliminary RWD styling and mobile menu interactions

--- source/themes/midwestphp/mwphp15-theme/assets/js/main.js
+++ source/themes/midwestphp/mwphp15-theme/assets/js/main.js
@@ -1,6 +1,37 @@
 jQuery.noConflict();
 
-jQuery(document).ready(function() {
+/*------------||
+|| Navigation ||
+||------------*/
+function toggleNav() {
+    jQuery('.menu-toggle').click(function() {
+
+        var navWidth,
+            mobileNavMenu = jQuery('.mobile-nav-menu'),
+            pageWrapper = jQuery('.wrapper'),
+            menuSpeed = 250;
+
+        // Copy navigation to mobile once when needed, and move register button to top of list
+        if (!mobileNavMenu.find('.menu').length > 0) {
+            jQuery('.primary-nav').find('.menu').clone().appendTo('.mobile-nav-menu');
+            mobileNavMenu.find('.register').prependTo(mobileNavMenu.find('.menu'));
+        }
+
+        // Show and hide navigation
+        if (pageWrapper.hasClass('open')) {
+            pageWrapper.removeClass('open').animate({left: '0', right: '0'}, menuSpeed);
+        } else {
+            navWidth = mobileNavMenu.width();
+            pageWrapper.addClass('open').animate({left: '-'+navWidth+'px', right: navWidth+'px'}, menuSpeed);
+        }
+    });
+}
+
+jQuery(function() {
+
+    jQuery('body').removeClass('no-js').addClass('js');
+
+    jQuery('.menu-toggle').on('click',toggleNav());
 
 });
 
